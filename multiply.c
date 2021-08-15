c13b81b4a24d45c916e88a98c09e3dad8cc70e59
Rakteem Mullick <rm1011.coding@gmail.com>
2021-08-15 22:31:12 +0530

    mulyiply.c

---
+++ multiply.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2;
+
+    printf("Enter the number");
+    scanf("%d%d",&num1,&num2);
+
+   int sum;
+
+   sum=num1*num2;
+
+   printf("The number  %d",sum);
+   return 0;
+}
